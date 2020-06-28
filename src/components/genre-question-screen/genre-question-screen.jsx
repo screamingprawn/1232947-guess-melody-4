8e553bbc09069364e7fbb679db198ccb23bfbf71
screamingprawn <unguessed@yandex.ru>
2020-06-28 09:09:45 +0300

    Add render function to component

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -16,8 +16,31 @@ class GenreQuestionScreen extends PureComponent {
     const {variants, genre} = question;
 
     return (
-      <div> </div>
-    );
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+          </a>
+
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: url(#blur);
+                      transform: rotate(-90deg) scaleY(-1);
+                      transform-origin: center}}/>
+          </svg>
+
+          <div className="game__mistakes">
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Выберите {genre} треки</h2>
+        </section>
+      </section>);
   }
 }
 
