0f0d9972aa4202d380e4625f6552b5232a3b45d3
screamingprawn <unguessed@yandex.ru>
2020-06-28 10:12:21 +0300

    Add form to the GuessGenre component

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -14,6 +14,7 @@ class GenreQuestionScreen extends PureComponent {
   render() {
     const {onAnswer, question} = this.props;
     const {variants, genre} = question;
+    const userAnswers = this.state.answers;
 
     return (
       <section className="game game--genre">
@@ -25,9 +26,9 @@ class GenreQuestionScreen extends PureComponent {
 
           <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
             <circle className="timer__line" cx="390" cy="390" r="370"
-              style={{filter: url(#blur);
-                      transform: rotate(-90deg) scaleY(-1);
-                      transform-origin: center}}/>
+              style={{filter: `url(#blur)`,
+                transform: `rotate(-90deg) scaleY(-1)`,
+                transformOrigin: `center`}}/>
           </svg>
 
           <div className="game__mistakes">
@@ -39,6 +40,37 @@ class GenreQuestionScreen extends PureComponent {
 
         <section className="game__screen">
           <h2 className="game__title">Выберите {genre} треки</h2>
+          <form className="game__tracks">
+            {variants.map((variant, i) => (
+              <div key={`${i}-${variant.src}`} className="track">
+                <button className="track__button track__button--play" type="button"/>
+                <div className="track__status">
+                  <audio src={variant.src} />
+                </div>
+                <div className="game__answer">
+                  <input className="game__input visually-hidden"
+                    type="checkbox"
+                    name="userAnswer"
+                    value={`variant-${i}`}
+                    id={`variant-${i}`}
+                    checked={userAnswers[i]}
+                    onChange={(evt) => {
+                      const value = evt.target.checked;
+
+                      this.setState({
+                        answers: [...userAnswers.slice(0, i),
+                          value,
+                          ...userAnswers.slice(i + 1)],
+                      });
+                    }}
+                  />
+                  <label className="game__check" htmlFor={`variant-${i}`}>Отметить</label>
+                </div>
+              </div>
+            ))}
+
+            <button className="game__submit button" type="submit">Ответить</button>
+          </form>
         </section>
       </section>);
   }
