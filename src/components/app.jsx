36aa0aba18321fd148141b6a230690a4defbc22b
screamingprawn <unguessed@yandex.ru>
2020-07-04 16:40:27 +0300

    Show WelcomeScreen if no screen was selected

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,21 +7,34 @@ class App extends PureComponent {
     super(props);
 
     this.state = {
-     step: -1, // показываем, что никакой экран ещё не включен
+      step: -1, // показываем, что никакой экран ещё не включен
+    };
+  }
+
+  _renderGameScreen() {
+    const mistakes = this.props.mistakes;
+    const handler = () => {
+      this.setState({step: 0});
+    };
+    const step = this.state.step;
+
+    if (step === -1) {
+      return (
+        <WelcomeScreen handler = {handler}
+          mistakes = {mistakes}/>);
+    } else {
+      return null;
     }
-  };
+  }
 
   render() {
-    return (
-      <WelcomeScreen
-        mistakes ={this.props.mistakes}
-        handler = {this.props.handler}/>);
+    return this._renderGameScreen();
   }
 }
 
+
 App.propTypes = {
-  mistakes: PropTypes.number.isRequired,
-  handler: PropTypes.func.isRequired
+  mistakes: PropTypes.number.isRequired
 };
 
 export {App};
--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import {App} from './components/app.jsx';
 
 const mistakes = 3;
-const handler = () => {};
 
 function init() {
   ReactDOM.render(
-      <App mistakes={mistakes}
-        handler={handler}/>,
+      <App mistakes={mistakes}/>,
       document.querySelector(`#root`)
   );
 }
