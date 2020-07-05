02faf7c02e1f3b5930c757619fb5450b3dbf97d1
screamingprawn <unguessed@yandex.ru>
2020-07-06 00:58:40 +0300

    Drop header from the game screen components

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -7,59 +7,39 @@ const ArtistQuestionScreen = (props) => {
   const {variants, song} = question;
 
   return (
-    <section className="game game--artist">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
-
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-        </svg>
-
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
-      </header>
-
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            <button className="track__button track__button--play" type="button" />
-            <div className="track__status">
-              <audio src={song.src}/>
-            </div>
+    <section className="game__screen">
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
+      <div className="game__track">
+        <div className="track">
+          <button className="track__button track__button--play" type="button" />
+          <div className="track__status">
+            <audio src={song.src}/>
           </div>
         </div>
-        <form className="game__artist">
-          {variants.map((variant, i) => (
-            <div className="artist"
-              key={variant.artist}>
-              <input className="artist__input visually-hidden"
-                type="radio"
-                name="artistAnswer"
-                value={`variant-${i}`}
-                id={`variant-${i}`}
-                onChange={(evt) => {
-                  evt.preventDefault();
-                  onAnswer(question, variant);
-                }}
-              />
-              <label className="artist__name" htmlFor={`variant-${i}`}>
-                <img className="artist__picture"
-                  src={variant.picture}
-                  alt={variant.artist}/>
-                {variant.artist}
-              </label>
-            </div>
-          ))}
-        </form>
-      </section>
+      </div>
+      <form className="game__artist">
+        {variants.map((variant, i) => (
+          <div className="artist"
+            key={variant.artist}>
+            <input className="artist__input visually-hidden"
+              type="radio"
+              name="artistAnswer"
+              value={`variant-${i}`}
+              id={`variant-${i}`}
+              onChange={(evt) => {
+                evt.preventDefault();
+                onAnswer(question, variant);
+              }}
+            />
+            <label className="artist__name" htmlFor={`variant-${i}`}>
+              <img className="artist__picture"
+                src={variant.picture}
+                alt={variant.artist}/>
+              {variant.artist}
+            </label>
+          </div>
+        ))}
+      </form>
     </section>
   );
 };
