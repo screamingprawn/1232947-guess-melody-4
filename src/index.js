6ec9dc2622c5d145c707997606b0a06e7b8ec8cb
screamingprawn <unguessed@yandex.ru>
2020-07-07 17:16:06 +0300

    Plugin Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 const mistakes = 3;
 
 function init() {
