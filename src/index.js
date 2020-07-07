e37d0117f359bd6f262a23a1953383ccf0fe48ae
screamingprawn <unguessed@yandex.ru>
2020-07-07 17:15:02 +0300

    Add store and reducer to the app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/app.jsx';
 import questions from './mocks/questions.js';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer.js";
 
+const store = createStore(reducer);
 const mistakes = 3;
 
 function init() {
   ReactDOM.render(
-      <App mistakes={mistakes}
-        questions={questions}/>,
+      <Provider store={store}>
+      <App
+        errorsCount={mistakes}
+        questions={questions}/>
+      </Provider>,
       document.querySelector(`#root`)
   );
 }
