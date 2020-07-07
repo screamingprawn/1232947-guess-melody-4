faa7ea38d3cf628cceed1a5056e34e40849f0a3f
screamingprawn <unguessed@yandex.ru>
2020-07-07 18:49:07 +0300

    Export connected component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './components/app.jsx';
+import App from './components/app.jsx';
 import questions from './mocks/questions.js';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
