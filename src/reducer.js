87bb0676dded58c5dc2a9d2ea89e51d20c22a80b
screamingprawn <unguessed@yandex.ru>
2020-07-08 12:19:15 +0300

    Add incrementMistake action to ActionCreator

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
 import {extend} from "./utils.js";
+import {GameType} from "./const.js";
 
 const initialState = {
   mistakes: 0,
@@ -15,7 +16,36 @@ const ActionCreator = {
     type: ActionType.INCREMENT_STEP,
     payload: 1,
   }),
+
+ incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  }
 };
+
+const isArtistAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.artist === question.song.artist;
+};
+
+const isGenreAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.every((it, i) => {
+    return it === (question.answers[i].genre === question.genre);
+  });
+};
+
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
